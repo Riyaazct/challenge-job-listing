9e3a95856d9fd34f3ff2905bc5e0739659e6abfc
Riyaaz Singh <riyaazct1@gmail.com>
2022-11-27 16:30:19 +0200

    add header image

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { ReactComponent as HeaderImg } from "./images/HeaderImg.svg";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <HeaderImg className="headerSvg" />
+    </Fragment>
+  );
+};
+
+export default Header;
