2c7983c35360d5d82f547abe9a2c6606295d9a5b
Riyaaz Singh <riyaazct1@gmail.com>
2022-11-29 21:52:27 +0200

    project completed

--- src/JobListings.js
+++ src/JobListings.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import data from "./data.json";
+import data from "./data/data.json";
 import "./JobListings.css";
 
 const JobListings = () => {
@@ -7,71 +7,78 @@ const JobListings = () => {
 
   return (
     <div className="container">
-      {jobData &&
-        jobData.map(
-          ({
-            company,
-            id,
-            logo,
-            featured,
-            position,
-            role,
-            level,
-            postedAt,
-            contract,
-            location,
-            languages,
-            tools,
-            new: newAd,
-          }) => {
-            return (
-              <div className="cardContainer" key={id}>
-                <div className="jobCard">
-                  <div className="imageContainer cardContent">
-                    <img src={logo} alt="" />
+      {jobData.map(
+        ({
+          company,
+          id,
+          logo,
+          featured,
+          position,
+          role,
+          level,
+          postedAt,
+          contract,
+          location,
+          languages,
+          tools,
+          new: newAd,
+        }) => {
+          return (
+            <div className="cardContainer" key={id}>
+              <div className="jobCard">
+                <div className="imageContainer cardContent">
+                  <img src={logo} alt="" />
+                </div>
+                <div className="cardContent centerContent">
+                  <div className="centerTitle">
+                    <li className="cardListItem company">
+                      {company}
+                    </li>
+                    <li
+                      className={newAd ? "isNewAd" : "cardListItem"}
+                    >
+                      {newAd ? "New!" : ""}
+                    </li>
+                    <li
+                      className={
+                        featured ? "featured" : "cardListItem"
+                      }
+                    >
+                      {featured ? "Featured" : ""}
+                    </li>
                   </div>
-                  <div className="cardContent centerContent">
-                    <div className="centerTitle">
-                      <li className="cardListItem company">
-                        {company}
-                      </li>
-                      <li className="cardListItem isNewAd">
-                        {newAd ? "New" : ""}
-                      </li>
-                      <li className="cardListItem featured">
-                        {featured ? "Featured" : ""}
-                      </li>
-                    </div>
-                    <div className="centerBody">
-                      <li className="cardListItem" position>
-                        {position}
-                      </li>
-                    </div>
-                    <div className="centerFoot">
-                      <li className="cardListItem postedAt">
-                        {postedAt}
-                      </li>
-                      <li className="cardListItem end contract">
-                        {contract}
-                      </li>
-                      <li className="cardListItem end">{location}</li>
-                    </div>
+                  <div className="centerBody">
+                    <li className="cardListItem" position>
+                      {position}
+                    </li>
                   </div>
-                  <div className="cardContent endContent">
-                    <li>{role}</li>
-                    <li>{level}</li>
-
-                    {languages.map((lang, index) => (
-                      <li key={index}>{lang}</li>
-                    ))}
-
-                    <li>{tools}</li>
+                  <div className="centerFoot">
+                    <li className="cardListItem postedAt">
+                      {postedAt}
+                    </li>
+                    <li className="cardListItem end contract">
+                      {contract}
+                    </li>
+                    <li className="cardListItem end">{location}</li>
                   </div>
                 </div>
+                <div className="cardContent endContent">
+                  <li>{role}</li>
+                  <li>{level}</li>
+
+                  {languages.map((lang, index) => (
+                    <li key={index}>{lang}</li>
+                  ))}
+
+                  {tools.map((tool, index) => (
+                    <li key={index}>{tool}</li>
+                  ))}
+                </div>
               </div>
-            );
-          }
-        )}
+            </div>
+          );
+        }
+      )}
     </div>
   );
 };
