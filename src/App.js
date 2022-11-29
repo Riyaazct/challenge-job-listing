844b83ad599f527cc9bbfe7631dd65efad5c8400
Riyaaz Singh <riyaazct1@gmail.com>
2022-11-29 22:13:57 +0200

    add component for footer and style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Header";
 import JobListings from "./JobListings";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <JobListings />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <Fragment>
+      <div class="attribution footer">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Riyaaz Singh</a>.
+      </div>
+    </Fragment>
+  );
+}
+
+export default Footer;
